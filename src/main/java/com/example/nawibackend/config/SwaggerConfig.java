214ff281f7abca8e7c8cbc21c7488607442faf4e
Mussadiq <fayazmusadiq81@gmail.com>
2026-09-02 16:03:53 +0000

    Added swagger integratio and configurd the datasourse yml

--- src/main/java/com/example/nawibackend/config/SwaggerConfig.java
+++ src/main/java/com/example/nawibackend/config/SwaggerConfig.java
@@ -1,4 +1,17 @@
 package com.example.nawibackend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI nawiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Nawi Backend API")
+                        .description("API documentation for Nawi Backend")
+                        .version("v1"));
+    }
 }
